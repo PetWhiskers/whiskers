7eadd6e7cddc14303e22f953240bb79cc05386cf
Jeanmarie <jeanmarie@dynamicalperspectives.com>
2017-07-24 18:59:00 -0400

    updates to Main.js

--- app/components/Main.js
+++ app/components/Main.js
@@ -28,14 +28,14 @@ var Main = React.createClass({
                   <span className="icon-bar"></span>
                   <span className="icon-bar"></span>
                 </button>
-                <Link className="navbar-brand" to="/">NYT-React</Link>
+                <Link className="navbar-brand" to="/">Whiskers App</Link>
               </div>
 
               <div className="collapse navbar-collapse navbar-ex1-collapse">
                 <ul className="nav navbar-nav navbar-right">
                   {/* Using <Link> in place of <a> and "to" in place of "href" */}
                   <li><Link to="/search">Search</Link></li>
-                  <li><Link to="/saved">Saved Articles</Link></li>
+                  <li><Link to="/saved">Saved Pets</Link></li>
                 </ul>
               </div>
             </div>
@@ -43,8 +43,8 @@ var Main = React.createClass({
 
           {/* Jumbotron */}
           <div className="jumbotron">
-            <h2 className="text-center"><strong>(ReactJS) New York Times Article Scrubber</strong></h2>
-            <h3 className="text-center">Search for and save articles of interest.</h3>
+            <h2 className="text-center"><strong> [Whiskers - Pet Finder App - Name TBD]</strong></h2>
+            <h3 className="text-center"></h3>
           </div>
 
 
@@ -56,7 +56,7 @@ var Main = React.createClass({
             <hr />
             <p className="pull-right">
               <i className="fa fa-github" aria-hidden="true"></i>
-              Proudly built using React.js
+              [Insert Something Here?]
             </p>
           </footer>
         </div>
